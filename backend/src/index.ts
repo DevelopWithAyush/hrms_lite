90b2b88c10ce7f120ad78d9b5de73d971d17eaaf
DevelopWithAyush <developwithdubey@gmail.com>
2026-01-21 23:54:27 +0530

    fixing things

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,7 +17,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: config.clientUrl || '*',
+    origin: config.clientUrl ,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
